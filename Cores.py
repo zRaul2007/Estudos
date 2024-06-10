03f11b03d91f91cdf5fb663f5cd8a086c8c1a9c0
Raul Pedrogan de Moura <111205773+zRaul2007@users.noreply.github.com>
2024-06-10 17:11:23 -0300

    Create Cores.py

---
+++ Cores.py
@@ -0,0 +1,24 @@
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+n = int(input('Digite um número inteiro para ver sua tabuada: '))
+
+print(f'\n{color.BOLD}{color.RED}\nA tabuada de {n} é: {color.END}\n')
+print('*' * 15)
+
+for i in range(10):
+    if i < 9:
+        print(f'{color.YELLOW}{n}  x   {i + 1}  =  {n * (i+1)}{color.END}')
+    else:
+        print(f'{color.YELLOW}{n}  x  {i + 1}  =  {n * (i+1)}{color.END}')
+
+print('*' * 15)
