ab1e877ede3a66dd0c59f0068414775e72ca7488
Raul Pedrogan <111205773+zRaul2007@users.noreply.github.com>
2026-07-23 09:54:04 -0300

    Create pagamentoMsg.php

---
+++ PHP/pagamentoMsg.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <title>Compra realizada!</title>
+</head>
+
+<body>
+    <div class="w3-container w3-teal">
+    <h1>
+    <strong>PROMOÇÃO DE MÊS DE ANIVERSÁRIO</strong>
+    <?php
+        $desconto;
+        $economia;
+        ;
+        if $_POST['cmdPg'] == 'Cartão de Crédito'{
+            $desconto = "10%"
+        }
+        echo " ".$_POST['txtNome']."! <br>";
+    ?>
+    </h1>
+</body>
